fb35ffca59bb2da13c9228741b0b79d7f223de7c
Julia-Ponomarenko <yuliasha_02@mail.ru>
2021-03-31 23:06:39 +0300

    practice_3.4

--- src/Main.java
+++ src/Main.java
@@ -1,53 +1,149 @@
 public class Main {
+    private final Object monitor = new Object();
+    private volatile char currentLetter = 'A';
+
+
     public static void main(String[] args) {
-        readFile();
-        unionFiles ();
-        readPages ();
+        Main w = new Main();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        Thread t3 = new Thread(() -> {
+            w.printC();
+        });
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 
-    }
-    public static void readFile ()
-    {
-        BufferedInputStream in = new BufferedInputStream(new FileInputStream("demo.txt"));
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        int x;
-        while ((x = in.read()) != -1) {
-            out.write(x);
+    public void printA() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        monitor.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    monitor.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        byte[] arr = out.toByteArray();
-        System.out.println(Arrays.toString(arr));
-        in.close();
-        out.close();
     }
-    public static void unionFiles ()
-    {
-        ArrayList<InputStream> al = new ArrayList<>();
-        al.add (new FileInputStream ("1.txt"));
-        al.add (new FileInputStream ("2.txt"));
-        al.add (new FileInputStream ("3.txt"));
-        al.add (new FileInputStream ("4.txt"));
-        al.add (new FileInputStream ("5.txt"));
-        SequenceInputStream in = new SequenceInputStream(Collections.enumeraton (al));
-        ByteArrayOutputStream out = new ByteArrayOutputStream("nubers.txt");
-        int x;
-        while ((x = in.read()) != -1) {
-            System.out.println((char) x);
-        }
-        in.close();
-        out.close();
-    }
-    public static void readPages ()
-    {
-        int pageSize =  1800;
-        RandomAccessFile file = new RandomAccessFile("book.txt", "r");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите номер страницу ");
-        int pageNumber = sc.nextInt - 1;
-        file.seek(pageNumber * pageSize);
-        byte[] arr = new byte[1800];
-        file.read(arr);
-        System.out.println(Arrays.toString(arr));
-        file.close();
 
+    public void printB() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        monitor.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'C';
+                    monitor.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printC() {
+        synchronized (monitor) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'C') {
+                        monitor.wait();
+                    }
+                    System.out.print("C");
+                    currentLetter = 'A';
+                    monitor.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class MFU {
+        Object scan = new Object();
+        Object print = new Object();
+
+        public static void main(String[] args) {
+            MFU mfu = new MFU();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    mfu.print("Документ 1", 3);
+                }
+            }).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    mfu.scan("Документ 2", 7, 1);
+                }
+            }).start();
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    mfu.scan("Документ 3", 8, 2);
+                }
+            }).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    mfu.scan("Документ 4", 4, 3);
+                }
+            }).start();
+        }
+
+
+        public void print(String doc, int n) {
+            synchronized (print) {
+                System.out.println("Начало печати");
+                for (int i = 0; i < n; i++) {
+                    System.out.println(i);
+                }
+                System.out.println("Конец печати");
+            }
+
+        }
+
+        public void scan(String doc, int x, int caseNumber) {
+            synchronized (scan) {
+                System.out.println("Начало сканирования");
+                switch (caseNumber) {
+                    case 1:
+                        System.out.println("Сканирование в файл");
+                        for (int i = 0; i < x; i++) {
+                            System.out.println(i);
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Сканирование на почту");
+                        for (int i = 0; i < x; i++) {
+                            System.out.println(i);
+                        }
+                        break;
+                    case 3:
+                        synchronized (print) {
+                            System.out.println("Ксерокопия начало");
+                            for (int i = 0; i < x; i++) {
+                                System.out.println(i);
+                            }
+                            System.out.println("Ксерокопия конец");
+                            break;
+                        }
+                }
+                System.out.println("Конец сканирования");
+            }
+        }
     }
 }
-
